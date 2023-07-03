52f5f6768ad48cb99317881bf2715545a595f256
Вячеслав Ярошевский <v.yaroshevskyi@outlook.com>
2023-07-03 10:01:33 +0300

    Test commit

--- Lab2_in_Python.py
+++ Lab2_in_Python.py
@@ -3,6 +3,8 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def init():
@@ -77,6 +79,46 @@ def main():
 
         # Затримка для забезпечення потрібної кількості кадрів в секунду
         pygame.time.wait(10)
+
+def compute_intersection(surface1, surface2):
+    # Обчислити лінію перетину кривих поверхонь
+    # surface1 і surface2 - це ваші криві поверхні
+    # Результат повинен бути лінією перетину, представленою як масив точок
+
+    # Приклад: просто з'єднаємо початкову та кінцеву точки обох поверхонь
+    intersection = np.array([surface1[0], surface2[0], surface1[-1], surface2[-1]])
+
+    return intersection
+
+def visualize_intersection(intersection):
+    # Візуалізувати лінію перетину
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Розділити координати точок на окремі масиви x, y, z
+    x = intersection[:, 0]
+    y = intersection[:, 1]
+    z = intersection[:, 2]
+
+    # Відобразити лінію перетину
+    ax.plot(x, y, z, color='red', label='Intersection')
+
+    # Додаткове налаштування графіка
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.legend()
+
+    # Показати графік
+    plt.show()
+
+# Приклад використання
+surface1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])  # Припустимо, що це перша крива поверхня
+surface2 = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])  # Припустимо, що це друга крива поверхня
+
+intersection = compute_intersection(surface1, surface2)
+visualize_intersection(intersection)
 
 if __name__ == '__main__':
     main()
